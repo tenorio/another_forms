6e8c2804b5e5281de3d89e70278fddc409a2206c
Marcelo Tenorio de Souza <marcelllo.martin@gmail.com>
2020-02-06 12:01:37 -0300

    Generate API assets

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      resources :forms
+      resources :questions, only: [:create, :update, :destroy]
+      resources :answers, only: [:index, :show, :create, :destroy]
+    end
+  end
 end
